c36cda6d15d46553fadf94fd669a6107214eae90
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-29 14:47:22 +0100

    teste reading

--- crud_user.php
+++ crud_user.php
@@ -1,16 +1,52 @@
 <?php
 class User
 {
-    function adduser($email, $mdp, $nom, $tel, $adresse, $grade)
+    function adduser($email, $mdp, $nom, $telephone, $adresse, $grade)
     {
         try {
             include('./config/connect.php');
-            $sql = "INSERT INTO `user`( `email`, `mdp`, `nom`, `telephone`, `adresse`, `grade`) VALUES ($email, $mdp, $nom,$tel,$adresse,$grade)";
+            $sql = "INSERT INTO `user`( `email`, `mdp`, `nom`, `telephone`, `adresse`, `grade`) VALUES ('" . $email . "','" . $mdp . "', '" . $nom . "', '" . $telephone . "', '" . $adresse . "', '" . $grade . "')";
             // use exec() because no results are returned
             $conn->exec($sql);
             echo "New record created successfully";
         } catch (PDOException $e) {
             echo $sql . "<br>" . $e->getMessage();
         }
+    }
+    function deleteuser($id)
+    {
+        try {
+            include('./config/connect.php');
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // sql to delete a record
+            $sql = "DELETE FROM user WHERE id='" . $id . "'";
+
+            // use exec() because no results are returned
+            $conn->exec($sql);
+            echo "Record deleted successfully";
+        } catch (PDOException $e) {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+    }
+    function list_users()
+    {
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "web_mp";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT * FROM user";
+        $result = $conn->query($sql);
+        return $result;
     }
 }
--- teste.php
+++ teste.php
@@ -1,2 +1,25 @@
 <?php
 //include('./config/connect.php');
+include("crud_user.php");
+$us = new User();
+
+//$us->adduser("email", "mdp", "nom", "telephone", "adresse", "grade");
+$x = $us->list_users();
+?><?php
+    if ($x->num_rows > 0) {
+    ?>
+<table>
+    <tr>
+        <td>nom</td>
+    </tr>
+    <?php
+
+        while ($row = $x->fetch_assoc()) {
+    ?>
+    <tr>
+        <td><?= $row["nom"] ?></td>
+    </tr>
+    <?php }
+    }
+?>
+</table>
