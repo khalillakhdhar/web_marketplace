df4527d74e03c6b92dfde3637557cb5a78f94a5b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-30 18:44:26 +0100

    Update vendeurs.php

--- vendeurs.php
+++ vendeurs.php
@@ -1,10 +1,10 @@
 <?php
 include("imports.php");
 include("nav.php");
-   
-    include("./models/crud_user.php");
-        $us=new User();
-    $liste=$us->list_vendeur();
+
+include("./models/crud_user.php");
+$us = new User();
+$liste = $us->list_vendeur();
 ?>
 <div class="main-container" id="container">
     <div class="overlay"></div>
@@ -33,23 +33,26 @@ include("nav.php");
                                     </tr>
                                 </thead>
                                 <tbody>
-                                <?php
-                                  while ($row = $liste->fetch_assoc()) {
-                                ?>
+                                    <?php
+                                    while ($row = $liste->fetch_assoc()) {
+                                    ?>
                                     <tr>
                                         <td><?= $row["nom"] ?></td>
                                         <td><?= $row["email"] ?></td>
                                         <td><?= $row["telephone"] ?></td>
                                         <td><?= $row["adresse"] ?></td>
                                         <td>
-                                            <div class="t-dot bg-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Normal"></div>
+                                            <div class="t-dot bg-primary" data-toggle="tooltip" data-placement="top"
+                                                title="" data-original-title="Normal"></div>
                                         </td>
-                                        <td class="text-center"> <a href="./controllers/ClientController.php?id=<?=$row["id"]?>&type=vendeur" class="btn btn-danger">supprimer</a> </td>
+                                        <td class="text-center"> <a
+                                                href="./controllers/ClientController.php?id=<?= $row["id"] ?>&type=vendeur"
+                                                class="btn btn-danger">supprimer</a> </td>
                                     </tr>
-                                  <?php } ?>
+                                    <?php } ?>
                                 </tbody>
                                 <tfoot>
-                                     <tr>
+                                    <tr>
                                         <th>Nom</th>
                                         <th>email</th>
                                         <th>tel</th>
@@ -71,9 +74,9 @@ include("nav.php");
         <!--  END CONTENT AREA  -->
     </div>
     <script>
-        $(document).ready(function() {
-            App.init();
-        });
+    $(document).ready(function() {
+        App.init();
+    });
     </script>
     <script src="assets/js/custom.js"></script>
     <!-- END GLOBAL MANDATORY SCRIPTS -->
@@ -81,27 +84,27 @@ include("nav.php");
     <!-- BEGIN PAGE LEVEL SCRIPTS -->
     <script src="plugins/table/datatable/datatables.js"></script>
     <script>
-        $(document).ready(function() {
-            $('table.multi-table').DataTable({
-                "oLanguage": {
-                    "oPaginate": {
-                        "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
-                        "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
-                    },
-                    "sInfo": "Showing page _PAGE_ of _PAGES_",
-                    "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
-                    "sSearchPlaceholder": "Search...",
-                    "sLengthMenu": "Results :  _MENU_",
+    $(document).ready(function() {
+        $('table.multi-table').DataTable({
+            "oLanguage": {
+                "oPaginate": {
+                    "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
+                    "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
                 },
-                "stripeClasses": [],
-                "lengthMenu": [7, 10, 20, 50],
-                "pageLength": 7,
-                drawCallback: function() {
-                    $('.t-dot').tooltip({
-                        template: '<div class="tooltip status" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
-                    })
-                    $('.dataTables_wrapper table').removeClass('table-striped');
-                }
-            });
+                "sInfo": "Showing page _PAGE_ of _PAGES_",
+                "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+                "sSearchPlaceholder": "Search...",
+                "sLengthMenu": "Results :  _MENU_",
+            },
+            "stripeClasses": [],
+            "lengthMenu": [7, 10, 20, 50],
+            "pageLength": 7,
+            drawCallback: function() {
+                $('.t-dot').tooltip({
+                    template: '<div class="tooltip status" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+                })
+                $('.dataTables_wrapper table').removeClass('table-striped');
+            }
         });
+    });
     </script>
